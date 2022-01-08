22200a523aea729239c86f5f33381f4c2280611d
Everson Evo <evotreinamentos@gmail.com>
2022-01-07 21:25:51 -0300

    teste 3

--- api_juventude/index.js
+++ api_juventude/index.js
@@ -1,1 +1,14 @@
-console.log("Alô mundo!  Evooo")
+const express = require('express');
+const app = express();
+
+app.get('/' , function(req , res, next){
+
+   res.send('SEja bem vindo')
+
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Servidor rodando porta");
+});
+
+
